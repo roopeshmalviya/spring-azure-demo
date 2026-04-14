39c398f2208a2681b4c9d6ae092afb8992fe3141
Malviya <romalviy@m-txrhhjk03v.corp.capgemini.com>
2026-04-14 17:26:50 +0530

    first commit

---
+++ src/main/java/com/mycompany/spring_azure_demo/SpringAzureDemoApplication.java
@@ -0,0 +1,21 @@
+package com.mycompany.spring_azure_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, Azure!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
