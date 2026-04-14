3fc6ab25695b1c234b236bafcd60ec789ff97442
Malviya <romalviy@m-txrhhjk03v.corp.capgemini.com>
2026-04-14 18:36:37 +0530

    Second commit

--- src/main/java/com/mycompany/spring_azure_demo/SpringAzureDemoApplication.java
+++ src/main/java/com/mycompany/spring_azure_demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello, Azure!";
+		return "Hello From Azure VM!";
 	}
 
 	public static void main(String[] args) {
